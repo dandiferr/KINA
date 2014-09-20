d49b911b1d5ce992256352b1f8c39990666126b3
dandiferr <canto.andy@gmail.com>
2014-09-20 09:47:19 -0400

    Edison Files

---
+++ Keyboard_ino/Keyboard_ino.ino
@@ -0,0 +1,39 @@
+void setup() {
+	Serial.begin(115200); // use the same baud-rate as the python side
+
+}
+void loop() {
+      if(digitalRead(4) == 1)
+      {
+        Serial.println("1");
+        while(digitalRead(4) == 1)
+        {
+           delay(10);
+        }
+	  }
+      else if(digitalRead(3) == 1)
+      {
+        Serial.println("2");
+        while(digitalRead(3) == 1)
+        {
+           delay(10);
+        }
+      }
+      else if(digitalRead(2) == 1)
+      {
+        Serial.println("3");
+        while(digitalRead(2) == 1)
+        {
+           delay(10);
+        }
+      }
+      else if(digitalRead(5) == 1)
+      {
+        Serial.println("4");
+        while(digitalRead(5) == 1)
+        {
+           delay(10);
+        }
+      }
+      delay(100);
+}
